db31cb5a52d30a968d1e71f9b503e5bd2085b2a6
hamed Mohammedi <hammedmohamadi@gmail.com>
2018-08-07 16:21:34 +0430

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace seccond
+{
+    public class Startup
+    {
+        private static counter co = new counter();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            
+            app.Use(async (c, n) =>
+            {
+                co.count++;
+                await n.Invoke();
+            });
+            app.Map("/count", app2 =>
+            {
+                app2.Run(async c =>
+                {
+                    c.Response.ContentType = "application/json";
+                    await c.Response.WriteAsync(co.ToString());
+                });
+            });
+            //agar ke req property khasti dasht vared mishavad
+            app.MapWhen(c => c.Request.Headers.ContainsKey("h"), app2 =>
+             {
+                 app2.Use(async (c, next) =>
+                 {
+                     await c.Response.WriteAsync("have h header.");
+                     await next.Invoke();
+                 });
+             });
+
+            //zamani ke url shamel masir khasi shavad pardazesh req vared in mishavad
+            app.Map("/map", app2 =>
+             {
+                 app2.Map("/map2", app3 =>
+                 {
+                     app3.Run(async c =>
+                     {
+                         await c.Response.WriteAsync("map3");
+                     });
+                 });
+                 app2.Use(async (c, next) =>
+                 {
+                     await c.Response.WriteAsync("<h1>map </h1>");
+                     await next.Invoke();
+                 });
+                 app2.Run(async c =>
+                 {
+                     await c.Response.WriteAsync("map2");
+                 });
+             });
+
+            //req ra ejra karde  va sepas be MW badi pass midahad.
+            app.Use(async (c, next) => 
+            {
+                 await c.Response.WriteAsync("<h1>Use </h1>");
+                 await next.Invoke();
+            });
+
+            //be ejra req mipardazad va pasokh ro bar migardanad.
+            app.Run(async c =>
+            {
+                await c.Response.WriteAsync("First MW!");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("seccond MW");
+            });
+        }
+    }
+}
---
+++ counter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace seccond
+{
+    public class counter
+    {
+        public int count { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("{");
+            sb.AppendLine();
+            sb.Append("count :");
+            sb.Append(count);
+            sb.AppendLine();
+            sb.Append("}");
+            return sb.ToString();
+        }
+    }
+}
